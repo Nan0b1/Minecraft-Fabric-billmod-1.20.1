60576b1d95d66ed88a7e5425cf98188d569622d9
Nan0b1 <pikajoss2009@gmail.com>
2025-01-05 16:17:34 +0100

    commit just at the start of ading an item

---
+++ src/main/java/net/nanobi/billmod/BillmodClient.java
@@ -0,0 +1,10 @@
+package net.nanobi.billmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BillmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/nanobi/billmod/BillmodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nanobi.billmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BillmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
